60f8b9bca971b47825a0ef484ea2536f7b37deef
sam <nedwors@gmail,com>
2020-11-26 12:55:57 +0000

    Refactors to abstract class

--- src/Pluralize/Utilities/Output.php
+++ src/Pluralize/Utilities/Output.php
@@ -5,11 +5,10 @@ namespace Nedwors\Pluralize\Pluralize\Utilities;
 use Closure;
 use Nedwors\Pluralize\Pluralize\Utilities\Container\Bindings;
 
-class Output
+class Output extends Vendor
 {
     protected Bindings $bindings;
     protected Parser $parser;
-    protected $output = null;
 
     public function __construct(Bindings $bindings, Parser $parser)
     {
@@ -17,66 +16,16 @@ class Output
         $this->parser = $parser;
     }
 
-    public function set($output)
-    {
-        $this->output = $output ?? $this->output;
-    }
-
     public function get($pluralString, $count, $singularString)
     {
-        $result = $this->generate($pluralString, $count, $singularString);
+        $result = $this->generate($singularString, $pluralString, $count);
 
-        if ($result) {
-            $result = $this->parser->run($result, $count);
+        if (!$result) {
+            $result = "$count $pluralString";
         }
 
-        $this->output = null;
+        $result = $this->parser->run($result, $count);
+        $this->userRequest = null;
         return $result;
-    }
-
-    protected function generate($pluralString, $count, $singularString)
-    {
-        if (is_callable($this->output)) {
-            return call_user_func($this->output, $pluralString, $count);
-        }
-
-        if (is_string($this->output)) {
-            return $this->getFromString($pluralString, $count);
-        }
-
-        return $this->getDefault($pluralString, $count, $singularString);
-    }
-
-    protected function getFromString($pluralString, $count)
-    {
-        if ($this->bindings->has($this->output)) {
-            return $this->resolveBinding($this->output, $pluralString, $count);
-        }
-
-        return $this->output;
-    }
-
-    protected function getDefault($pluralString, $count, $singularString)
-    {
-        if ($this->bindings->has($singularString)) {
-            return $this->resolveBinding($singularString, $pluralString, $count);
-        }
-
-        if ($this->bindings->has(Bindings::DEFAULT)) {
-            return $this->resolveBinding(Bindings::DEFAULT, $pluralString, $count);
-        }
-
-        return "$count $pluralString";
-    }
-
-    protected function resolveBinding($output, ...$params)
-    {
-        $concrete = $this->bindings->get($output);
-
-        if ($concrete instanceof Closure) {
-            return call_user_func($concrete, ...$params);
-        }
-
-        return $concrete;
     }
 }
---
+++ src/Pluralize/Utilities/Vendor.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Nedwors\Pluralize\Pluralize\Utilities;
+
+use Nedwors\Pluralize\Pluralize\Utilities\Container\Bindings;
+
+abstract class Vendor
+{
+    protected $userRequest = null;
+
+    public function set($userRequest)
+    {
+        $this->userRequest = $userRequest ?? $this->userRequest;
+    }
+
+    protected function generate($singularString, ...$params)
+    {
+        return $this->getFromUserRequest(...$params) ?? $this->getDefault($singularString, ...$params);
+    }
+
+    protected function getFromUserRequest(...$params)
+    {
+        if (is_callable($this->userRequest)) {
+            return $this->runClosure($this->userRequest, ...$params);
+        }
+
+        if (is_string($this->userRequest)) {
+            return $this->getFromString(...$params);
+        }
+    }
+
+    protected function getFromString(...$params)
+    {
+        return $this->canBeResolved($this->userRequest)
+            ? $this->resolve($this->userRequest, ...$params)
+            : $this->userRequest;
+    }
+
+    protected function getDefault($singularString, ...$params)
+    {
+        if ($this->canBeResolved($singularString)) {
+            return $this->resolve($singularString, ...$params);
+        }
+
+        if ($this->canBeResolved(Bindings::DEFAULT)) {
+            return $this->resolve(Bindings::DEFAULT, ...$params);
+        }
+    }
+
+    protected function canBeResolved($binding)
+    {
+        return $this->bindings->has($binding);
+    }
+
+    protected function resolve($output, ...$params)
+    {
+        return is_callable($concrete = $this->bindings->get($output)) 
+            ? $this->runClosure($concrete, ...$params)
+            : $concrete;
+    }
+
+    protected function runClosure($closure, ...$params)
+    {
+        return call_user_func($closure, ...$params);
+    }
+}
