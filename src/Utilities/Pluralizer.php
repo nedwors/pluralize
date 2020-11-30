cf40a4b678dceedac6bc22ae0c249c53b3d47d4c
Sam Rowden <59183434+nedwors@users.noreply.github.com>
2020-11-30 21:49:32 +0000

    Generation improvements (#9)
    * Refactors how pluralization occurs
    
    * Clean up
    
    * Apply fixes from StyleCI
    
    * Adds missing tests
    
    * Updates travis.yml
    
    * Apply fixes from StyleCI
    
    Co-authored-by: sam <nedwors@gmail,com>
    Co-authored-by: Sam Rowden <nedwors@users.noreply.github.com>

---
+++ src/Utilities/Pluralizer.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Nedwors\Pluralize\Utilities;
+
+use Stringable;
+
+class Pluralizer implements Stringable
+{
+    protected Engine $engine;
+    protected string $singular;
+    protected $pluralizationDriver;
+
+    public function __construct(Engine $engine)
+    {
+        $this->engine = $engine;
+    }
+
+    /**
+     * Begin the process with the singular form of the word desired.
+     *
+     * @param string $singular
+     * @return Pluralizer
+     */
+    public static function pluralize(string $singular): self
+    {
+        $instance = app(self::class);
+        $instance->singular = $singular;
+
+        return $instance;
+    }
+
+    /**
+     * Define what should be counted.
+     *
+     * @param int|array|Collection|LengthAwarePaginator|Paginator $countable
+     * @return Pluralizer
+     */
+    public function from($countable): self
+    {
+        $this->engine->counter()->calculate($countable);
+
+        return $this;
+    }
+
+    /**
+     * Define the format of the generated string.
+     *
+     * @param string|Closure $output
+     * @return Pluralizer
+     */
+    public function as($output): self
+    {
+        $this->engine->output()->set($output);
+
+        return $this;
+    }
+
+    /**
+     * Define the format of the generated fallback string.
+     *
+     * @param string|Closure $fallback
+     * @return Pluralizer
+     */
+    public function or($fallback): self
+    {
+        $this->engine->fallback()->set($fallback);
+
+        return $this;
+    }
+
+    /**
+     * Declare the driver class to be used for producing plural strings.
+     *
+     * @param mixed $pluralizationDriver
+     * @return Pluralizer
+     */
+    public function using($pluralizationDriver): self
+    {
+        $this->pluralizationDriver = $pluralizationDriver;
+
+        return $this;
+    }
+
+    /**
+     * Generate and return the string.
+     *
+     * @return string
+     */
+    public function go()
+    {
+        return $this->countIsNull() ? $this->getFallback() : $this->getOutput();
+    }
+
+    protected function countIsNull()
+    {
+        return is_null($this->engine->counter()->count);
+    }
+
+    protected function getFallback()
+    {
+        return $this->engine->fallback()->get($this->pluralized(), $this->singular);
+    }
+
+    protected function getOutput()
+    {
+        return $this->engine->output()->get($this->pluralized(), $this->engine->counter()->count, $this->singular);
+    }
+
+    protected function pluralized()
+    {
+        return $this->engine->pluralization($this->pluralizationDriver)
+                            ->run($this->singular, $this->engine->counter()->count);
+    }
+
+    public function __invoke()
+    {
+        return $this->go();
+    }
+
+    public function __toString()
+    {
+        return $this->go();
+    }
+}
--- tests/BindingTest.php
+++ tests/BindingTest.php
@@ -62,4 +62,30 @@ class BindingTest extends TestCase
         $string = Pluralize::this('Book')->from(10);
         $this->assertEquals('Currently, 10 Books', $string);
     }
+
+    /** @test */
+    public function a_default_fallback_binding_can_be_bound_following_a_specific_binding()
+    {
+        Pluralize::bind('test')->fallback('A test fallback');
+        Pluralize::bind()->fallback('Oops!');
+
+        $string = Pluralize::this('Book')->from(null);
+        $this->assertEquals('Oops!', $string);
+
+        $string = Pluralize::this('Book')->from(null)->or('test');
+        $this->assertEquals('A test fallback', $string);
+    }
+
+    /** @test */
+    public function a_default_output_binding_can_be_bound_following_a_specific_binding()
+    {
+        Pluralize::bind('test')->output('A test output');
+        Pluralize::bind()->output(fn ($plural, $count) => "Here is|are $count $plural");
+
+        $string = Pluralize::this('Book')->from(10);
+        $this->assertEquals('Here are 10 Books', $string);
+
+        $string = Pluralize::this('Book')->from(10)->as('test');
+        $this->assertEquals('A test output', $string);
+    }
 }
